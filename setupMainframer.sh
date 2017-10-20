e0d59798b6883356d2a94d706f62e375cd507e71
Luiz Gustavo Pozzo <luiz.pozzo@travelportdigital.com>
2017-10-20 12:03:08 +0100

    restarting

---
+++ setupMainframer.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+set -e
+
+if [[ -z $1 ||  -z $2 ]]; then
+	echo "
+		this scrpt requires 2 parameters
+		\$1 REMOTE_MACHINE_USERNAME i.e: root (just kiding, not root pleas)
+		\$2 REMOTE_MACHINE_IP_OR_HOSTNAME i.e: 192.168.0.1"
+	exit;
+fi
+
+REMOTE_MACHINE_ALIAS="mainframer-server"
+REMOTE_MACHINE_USERNAME=$2
+REMOTE_MACHINE_IP_OR_HOSTNAME=$3
+
+if [ ! -f ~/.ssh/mainframer ]; then
+	mkdir -p ~/.ssh
+	chmod u+rw,go= ~/.ssh
+	ssh-keygen -f ~/.ssh/mainframer 
+fi;
+
+echo "Host $REMOTE_MACHINE_ALIAS
+  User $REMOTE_MACHINE_USERNAME
+  HostName $REMOTE_MACHINE_IP_OR_HOSTNAME
+  Port 22
+  IdentityFile ~/.ssh/mainframer
+  PreferredAuthentications publickey
+  ControlMaster auto
+  ControlPath /tmp/%r@%h:%p
+  ControlPersist 1h
+
+
+" >> ~/.ssh/config
+
+echo "remote_machine=$REMOTE_MACHINE_ALIAS" > ~/.mainframer
+
+sudo wget -O /usr/local/bin/mainframer https://raw.githubusercontent.com/gojuno/mainframer/development/mainframer
+sudo chmod +x /usr/local/bin/mainframer
+
+echo "Copy the next key and send to the server guy
+
+
+"
+cat ~/.ssh/mainframer.pub
+
